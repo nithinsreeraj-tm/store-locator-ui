a6bc453afb5d9d88c3d53323c625b209e4f9f822
Nithin S <nithin.s@terrificminds.com>
2023-08-05 14:41:04 +0530

    Changes to script

--- Gmaps/script.js
+++ Gmaps/script.js
@@ -4,246 +4,266 @@
 
 
 
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 40.749933, lng: -73.98633 },
-    zoom: 13,
-    mapTypeControl: false,
-  });
+let searchradius=5000;
+let category="store";
 
-  let markers=[];
+function changeRadius(){
+  searchradius=document.getElementById("radius").value;
+}
 
-
-  infoWindow = new google.maps.InfoWindow();
-
-
-
-          // Create a button to trigger the nearby restaurant search
-          const searchButton = document.createElement("button");
-          searchButton.textContent = "Search Nearby Restaurants";
-          searchButton.classList.add("custom-map-control-button");
-          map.controls[google.maps.ControlPosition.LEFT_CENTER].push(searchButton);
-          searchButton.addEventListener("click", () => {
-            searchNearbyRestaurants();
-          });
-        
-      
-          function searchNearbyRestaurants() {
-
-            clearMarkers();  
-            
-            if (navigator.geolocation) {
-              navigator.geolocation.getCurrentPosition(
-                (position) => {
-                  const pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude,
-                  };
-        
-                  const service = new google.maps.places.PlacesService(map);
-                  service.nearbySearch(
-                    {
-                      location: pos,
-                      radius: 5500, // You can adjust the radius as needed
-                      type: ["restaurant"],
-                    },
-                    (results, status) => {
-                      if (status === google.maps.places.PlacesServiceStatus.OK) {
-                        for (let i = 0; i < results.length; i++) {
-                          createMarker(results[i]);
-                          console.log(results[i]);
-                        }
-                      }
-                    }
-                  );
-                },
-                () => {
-                  handleLocationError(true, infoWindow, map.getCenter());
-                }
-              );
-            } else {
-              // Browser doesn't support Geolocation
-              handleLocationError(false, infoWindow, map.getCenter());
-            }
-          }
-
-          function createMarker(place) {
-            var placeLoc = place.geometry.location;
-            
-            var marker = new google.maps.Marker({
-                map : map,
-                position : place.geometry.location
-            });
-            markers.push(marker);
-
-            google.maps.event.addListener(marker, 'click', function() {
-                infowindow.setContent(place.name);
-                infowindow.open(map, this);
-            });
-        }
-
-        function clearMarkers(){
-          for (let i = 0; i < markers.length; i++) {
-            markers[i].setMap(null);
-            markers=[];
-          }
-        }
-
-
-  const locationButton = document.createElement("button");
-
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        },
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  });
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation.",
-  );
-  infoWindow.open(map);
+function changeCat(){
+  category=document.getElementById("category").value;
 }
 
 
-  const card = document.getElementById("pac-card");
-  const input = document.getElementById("pac-input");
-  const biasInputElement = document.getElementById("use-location-bias");
-  const strictBoundsInputElement = document.getElementById("use-strict-bounds");
-  const options = {
-    fields: ["formatted_address", "geometry", "name"],
-    strictBounds: false,
-  };
 
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
+function initMap() {
+    const map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 40.749933, lng: -73.98633 },
+      zoom: 13,
+      mapTypeControl: false,
+    });
 
-  const autocomplete = new google.maps.places.Autocomplete(input, options);
+    let markers=[];
+    let selectedpos;
 
-  // Bind the map's bounds (viewport) property to the autocomplete object,
-  // so that the autocomplete requests use the current map bounds for the
-  // bounds option in the request.
-  autocomplete.bindTo("bounds", map);
 
-  const infowindow = new google.maps.InfoWindow();
-  const infowindowContent = document.getElementById("infowindow-content");
+    infoWindow = new google.maps.InfoWindow();
 
-  infowindow.setContent(infowindowContent);
 
-  const marker = new google.maps.Marker({
-    map,
-    anchorPoint: new google.maps.Point(0, -29),
-  });
 
-  autocomplete.addListener("place_changed", () => {
-    infowindow.close();
-    marker.setVisible(false);
+            // Create a button to trigger the nearby restaurant search
+            const searchButton = document.createElement("button");
+            searchButton.textContent = "Search Nearby Restaurants";
+            searchButton.classList.add("custom-map-control-button");
+            map.controls[google.maps.ControlPosition.LEFT_CENTER].push(searchButton);
+            searchButton.addEventListener("click", () => {
+              searchNearbyRestaurants();
+            });
+          
+        
+            function searchNearbyRestaurants() {
 
-    const place = autocomplete.getPlace();
+              clearMarkers();  
+              
+              // if (navigator.geolocation) {
+              //   navigator.geolocation.getCurrentPosition(
+              //     (position) => {
+              //       const pos = {
+              //         lat: position.coords.latitude,
+              //         lng: position.coords.longitude,
+              //       };
+          
+                    const service = new google.maps.places.PlacesService(map);
+                    service.nearbySearch(
+                      {
+                        location: selectedpos,
+                        radius: searchradius, // You can adjust the radius as needed
+                        type: [category],
+                      },
+                      (results, status) => {
+                        if (status === google.maps.places.PlacesServiceStatus.OK) {
+                          for (let i = 0; i < results.length; i++) {
+                            createMarker(results[i]);
+                            console.log(results[i]);
+                          }
+                        }
+                      }
+                    );
+                //   },
+                //   () => {
+                //     handleLocationError(true, infoWindow, map.getCenter());
+                //   }
+                // );
+              // } else {
+              //   // Browser doesn't support Geolocation
+              //   handleLocationError(false, infoWindow, map.getCenter());
+              // }
+            }
 
-    if (!place.geometry || !place.geometry.location) {
-      // User entered the name of a Place that was not suggested and
-      // pressed the Enter key, or the Place Details request failed.
-      window.alert("No details available for input: '" + place.name + "'");
-      return;
+            function createMarker(place) {
+              var placeLoc = place.geometry.location;
+
+              selectedpos=place.geometry.location;
+              
+              var marker = new google.maps.Marker({
+                  map : map,
+                  position : place.geometry.location
+              });
+              markers.push(marker);
+  
+              google.maps.event.addListener(marker, 'click', function() {
+                  infowindow.setContent(place.name);
+                  infowindow.open(map, this);
+              });
+          }
+
+          function clearMarkers(){
+            for (let i = 0; i < markers.length; i++) {
+              markers[i].setMap(null);   
+            }
+            markers=[];
+          }
+
+
+    const locationButton = document.createElement("button");
+  
+    locationButton.textContent = "Pan to Current Location";
+    locationButton.classList.add("custom-map-control-button");
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+    locationButton.addEventListener("click", () => {
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+            selectedpos=pos;
+            clearMarkers();
+  
+            infoWindow.setPosition(pos);
+            infoWindow.setContent("Location found.");
+            infoWindow.open(map);
+            map.setCenter(pos);
+          },
+          () => {
+            handleLocationError(true, infoWindow, map.getCenter());
+          },
+        );
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+    });
+  
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+      browserHasGeolocation
+        ? "Error: The Geolocation service failed."
+        : "Error: Your browser doesn't support geolocation.",
+    );
+    infoWindow.open(map);
+  }
+
+
+    const card = document.getElementById("pac-card");
+    const input = document.getElementById("pac-input");
+    const biasInputElement = document.getElementById("use-location-bias");
+    const strictBoundsInputElement = document.getElementById("use-strict-bounds");
+    const options = {
+      fields: ["formatted_address", "geometry", "name"],
+      strictBounds: false,
+    };
+  
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
+  
+    const autocomplete = new google.maps.places.Autocomplete(input, options);
+  
+    // Bind the map's bounds (viewport) property to the autocomplete object,
+    // so that the autocomplete requests use the current map bounds for the
+    // bounds option in the request.
+    autocomplete.bindTo("bounds", map);
+  
+    const infowindow = new google.maps.InfoWindow();
+    const infowindowContent = document.getElementById("infowindow-content");
+  
+    infowindow.setContent(infowindowContent);
+    clearMarkers();
+    const marker = new google.maps.Marker({
+      map,
+      anchorPoint: new google.maps.Point(0, -29),
+    });
+  
+    autocomplete.addListener("place_changed", () => {
+      infowindow.close();
+      marker.setVisible(false);
+  
+      const place = autocomplete.getPlace();
+      selectedpos=place.geometry.location;
+  
+      if (!place.geometry || !place.geometry.location) {
+        // User entered the name of a Place that was not suggested and
+        // pressed the Enter key, or the Place Details request failed.
+        window.alert("No details available for input: '" + place.name + "'");
+        return;
+      }
+  
+      // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+      }
+  
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+      infowindowContent.children["place-name"].textContent = place.name;
+      infowindowContent.children["place-address"].textContent =
+        place.formatted_address;
+      infowindow.open(map, marker);
+    });
+  
+    // Sets a listener on a radio button to change the filter type on Places
+    // Autocomplete.
+    function setupClickListener(id, types) {
+      const radioButton = document.getElementById(id);
+  
+      radioButton.addEventListener("click", () => {
+        autocomplete.setTypes(types);
+        input.value = "";
+      });
     }
-
-    // If the place has a geometry, then present it on a map.
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-    }
-
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
-    infowindowContent.children["place-name"].textContent = place.name;
-    infowindowContent.children["place-address"].textContent =
-      place.formatted_address;
-    infowindow.open(map, marker);
-  });
-
-  // Sets a listener on a radio button to change the filter type on Places
-  // Autocomplete.
-  function setupClickListener(id, types) {
-    const radioButton = document.getElementById(id);
-
-    radioButton.addEventListener("click", () => {
-      autocomplete.setTypes(types);
+  
+    setupClickListener("changetype-all", []);
+    setupClickListener("changetype-address", ["address"]);
+    setupClickListener("changetype-establishment", ["establishment"]);
+    setupClickListener("changetype-geocode", ["geocode"]);
+    setupClickListener("changetype-cities", ["(cities)"]);
+    setupClickListener("changetype-regions", ["(regions)"]);
+    biasInputElement.addEventListener("change", () => {
+      if (biasInputElement.checked) {
+        autocomplete.bindTo("bounds", map);
+      } else {
+        // User wants to turn off location bias, so three things need to happen:
+        // 1. Unbind from map
+        // 2. Reset the bounds to whole world
+        // 3. Uncheck the strict bounds checkbox UI (which also disables strict bounds)
+        autocomplete.unbind("bounds");
+        autocomplete.setBounds({ east: 180, west: -180, north: 90, south: -90 });
+        strictBoundsInputElement.checked = biasInputElement.checked;
+      }
+  
       input.value = "";
     });
-  }
-
-  setupClickListener("changetype-all", []);
-  setupClickListener("changetype-address", ["address"]);
-  setupClickListener("changetype-establishment", ["establishment"]);
-  setupClickListener("changetype-geocode", ["geocode"]);
-  setupClickListener("changetype-cities", ["(cities)"]);
-  setupClickListener("changetype-regions", ["(regions)"]);
-  biasInputElement.addEventListener("change", () => {
-    if (biasInputElement.checked) {
-      autocomplete.bindTo("bounds", map);
-    } else {
-      // User wants to turn off location bias, so three things need to happen:
-      // 1. Unbind from map
-      // 2. Reset the bounds to whole world
-      // 3. Uncheck the strict bounds checkbox UI (which also disables strict bounds)
-      autocomplete.unbind("bounds");
-      autocomplete.setBounds({ east: 180, west: -180, north: 90, south: -90 });
-      strictBoundsInputElement.checked = biasInputElement.checked;
-    }
-
-    input.value = "";
-  });
-  strictBoundsInputElement.addEventListener("change", () => {
-    autocomplete.setOptions({
-      strictBounds: strictBoundsInputElement.checked,
+    strictBoundsInputElement.addEventListener("change", () => {
+      autocomplete.setOptions({
+        strictBounds: strictBoundsInputElement.checked,
+      });
+      if (strictBoundsInputElement.checked) {
+        biasInputElement.checked = strictBoundsInputElement.checked;
+        autocomplete.bindTo("bounds", map);
+      }
+  
+      input.value = "";
     });
-    if (strictBoundsInputElement.checked) {
-      biasInputElement.checked = strictBoundsInputElement.checked;
-      autocomplete.bindTo("bounds", map);
     }
+  
 
-    input.value = "";
-  });
-  }
-
+  
 
 
 
+  
+  window.initMap = initMap;
 
-
-
-window.initMap = initMap;
-
-// Note: This example requires that you consent to location sharing when
+  // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see the error "The Geolocation service
 // failed.", it means you probably did not give permission for the browser to
 // locate you.
 let map, infoWindow;
+
 
